4e5d4186c9eb9496ea525aa73e7c48437707125f
jingpengwu <jingpeng.wu@gmail.com>
2017-02-09 13:44:21 -0500

    add BOSSArrays.jl file

--- src/BOSSArrays.jl
+++ src/BOSSArrays.jl
@@ -1,12 +1,13 @@
+module BOSSArrays
+
 using PyCall
 PyCall.@pyimport intern.remote.boss as mb
 PyCall.@pyimport intern.resource.boss as sb
 PyCall.@pyimport numpy
 
-module BOSS
-
 export BOSSArray
 
+# include("base.jl")
 const DEFAULT_COLL_NAME = "team2_waypoint"
 const DEFAULT_EXP_NAME = "pinky10"
 const DEFAULT_CHAN_NAME = "em"
@@ -15,8 +16,8 @@ const DEFAULT_DATA_TYPE = "uint16"
 const DEFAULT_RESOLUTION_LEVEL = 0
 
 immutable BOSSArray <: AbstractArray
-    rmt     :: PyObject
-    chan    :: PyObject
+    rmt     :: PyCall.PyObject
+    chan    :: PyCall.PyObject
 end
 
 function BOSSArray( collectionName  = DEFAULT_COLL_NAME,
@@ -43,6 +44,11 @@ function Base.getindex(ba::BOSSArray, idxes::Union{UnitRange, Int} ...)
     y_rng = [idxes[2].start, idxes[2].stop]
     z_rng = [idxes[3].start, idxes[3].stop]
 
-    return rmt[:get_cutout](chan, DEFAULT_RESOLUTION_LEVEL,
+    data = rmt[:get_cutout](chan, DEFAULT_RESOLUTION_LEVEL,
                 x_rng, y_rng, z_rng)
+    data = permutedims(data, [3,2,1])
+    return data
+end
+
+
 end
